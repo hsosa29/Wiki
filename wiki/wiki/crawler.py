8bf13d9dc26fec977cae4944b9a0d9b2086686d4
hsosa <hsosa@beesion.com>
2020-02-10 17:41:12 -0600

    Add crawler file

--- wiki/wiki/crawler.py
+++ wiki/wiki/crawler.py
@@ -4,32 +4,31 @@ import json
 import os
 import string
 
-def obtenerCuentaPalabras(ruta_actual,nombre_arc_act):
+def getPalabras(ruta_actual,nombre_arc_act):
     soup=BeautifulSoup(open(ruta_actual),"html.parser")
     for script in soup(["script","style"]):
       script.extract()
     texto_completo = soup.get_text()
     try:
         titulo=soup.find('title').get_text()        
-        print 'extrayendo titilo '+titulo
+        print ('extrayendo titulo '+titulo)
     except:
         titulo='Sin titulo'
     pageRankArchivo[url_act]['titulo']=titulo
 
-    #Se revisa si existe la carpeta para escribir los archivos y sino se crea la carpeta
+    #Se crea la carpeta pags_texto
     carpeta='pags_texto'
     if not os.path.exists(carpeta):
       os.makedirs(carpeta)
-    # Se escribe el archivo con el texto plano
-    archivo_texto_pl=open(carpeta+'/'+nombre_arc_act+'.txt','w')
+   
+    text_file=open(carpeta+'/'+nombre_arc_act+'.txt','w')
     print texto_completo
     textoSinCarEsp = texto_completo.translate ({ord(c): None for c in "↑!@#$%^&*()[]{};:,./<>?\|`~-=_+"})
     print textoSinCarEsp
-    #textoSinCarEsp = texto_completo.translate(table)
-     
-    archivo_texto_pl.write(textoSinCarEsp.encode('utf-8'))
-    archivo_texto_pl.close()
-    #Ahora extraemos la lista de palabras de cada archivo y armamos un diccionario python con las frecuencias. 
+        
+    text_file.write(textoSinCarEsp.encode('utf-8'))
+    text_file.close()
+    #Armar diccionario a partir del archivo de texto plano.
     listaPalabras = textoSinCarEsp.split()
     frecPalabras={}
     for palabra in listaPalabras:
@@ -37,20 +36,18 @@ def obtenerCuentaPalabras(ruta_actual,nombre_arc_act):
 
     return frecPalabras
 
-
-
 archivo_pags=open('pageRankOut.json','r').read()
 pageRankArchivo=json.loads(archivo_pags)
 lista_pags=list(pageRankArchivo.keys())
 print len(lista_pags)
 pags_index=0
-# Ciclo para leer todos las páginas
+# Lectura de todas las páginas
 for url_act in lista_pags:
   ruta_temp = pageRankArchivo[url_act]['ruta']
   print ruta_temp
   nombre_archivo = ruta_temp.split('pags_html/')[1]
   ruta_temp = 'pags_html/'+nombre_archivo
-  calif_pags=obtenerCuentaPalabras(ruta_temp,nombre_archivo.split('.html')[0])
+  calif_pags=getPalabras(ruta_temp,nombre_archivo.split('.html')[0])
   pageRankArchivo[url_act]['palabras']=calif_pags
   pags_index+=1
   print('Páginas indexadas: '+str(pags_index))
